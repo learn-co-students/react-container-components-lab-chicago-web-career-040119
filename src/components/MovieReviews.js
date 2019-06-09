4b9664344e70c59f0a83c43c06580b598a76e5b5
Sarah Elzinga <sevans924@gmail.com>
2019-06-09 13:25:20 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+return(
+  <div className="review-list">
+    {reviews.map(review =>
+      <div class="review">
+      <p>{review.author}</p>
+      </div>
+    )}
+    </div>
+)
+}
+
+export default MovieReviews
